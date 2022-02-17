03d46ddba54142efe6a2e6296736c7138ac25808
Goutham S <gouthamshine@gmail.com>
2022-02-17 15:58:37 +0530

    added nodejs

--- hcj/webstash-10/app/script.js
+++ hcj/webstash-10/app/script.js
@@ -1,18 +1,18 @@
-const jokeEle = document.getElementById("joke");
-const jokeBtn = document.getElementById("jokeBtn");
+const jokeEle = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
 
-jokeBtn.addEventListener("click", fetchJoke);
+jokeBtn.addEventListener('click', fetchJoke);
 fetchJoke();
 
 async function fetchJoke() {
-  const header = {
-    headers: {
-      Accept: "application/json",
-    },
-  };
-  const res = await fetch("https://icanhazdadjoke.com", header);
-  const data = await res.json();
-  jokeEle.innerHTML = data.joke;
+    const header = {
+        headers: {
+            Accept: 'application/json',
+        },
+    };
+    const res = await fetch('https://icanhazdadjoke.com', header);
+    const data = await res.json();
+    jokeEle.innerHTML = data.joke;
 }
 
 // function fetchJoke() {
